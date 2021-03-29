9a95f5dfc0ede207f104dc628e5e97ee3d0cfdbd
tahapaksu <taha@nox.ist>
2021-03-29 13:36:01 +0000

    translation

--- views/backend/index.blade.php
+++ views/backend/index.blade.php
@@ -1,21 +1,19 @@
 @extends($extends)
 @section("content")
-<div class="p-8 border-t min-h-screen">
-    <div class="w-full items-end">
+<div class="min-h-screen p-8 border-t">
+    <div class="items-end w-full">
         <div class="flex">
             <div class="w-2/3">
-                <h2 class="text-3xl font-semibold mb-3" style="line-height: 1em">@lang("Common Questions & Answers")</h2>
+                <h2 class="mb-3 text-3xl font-semibold" style="line-height: 1em">@lang("Common Questions and Answers")</h2>
             </div>
             <div class="w-1/3 text-right">
                 <a href="{{route('paksuco.faq.create')}}"
-                class="shadow bg-indigo-500 hover:bg-indigo-400 whitespace-no-wrap
-                focus:shadow-outline focus:outline-none text-white font-normal
-                py-2 px-3 rounded">
-                    <i class="fa fa-plus mr-2"></i>@lang("Create a new FAQ Item")
+                class="px-3 py-2 font-normal text-white whitespace-no-wrap bg-indigo-500 rounded shadow hover:bg-indigo-400 focus:shadow-outline focus:outline-none">
+                    <i class="mr-2 fa fa-plus"></i>@lang("Create a new FAQ Item")
                 </a>
             </div>
         </div>
-        <p class="text-gray-600 font-light leading-5 mb-4 text-sm">Lorem ipsum dolor sit amet, consectetur
+        <p class="mb-4 text-sm font-light leading-5 text-gray-600">Lorem ipsum dolor sit amet, consectetur
             adipiscing elit. Proin interdum urna sit amet lorem iaculis, aliquet suscipit sapien venenatis.
             Sed congue vitae velit vitae varius. Mauris egestas consequat mauris sit amet mollis. Proin porta
             tortor in urna tincidunt vehicula. Integer urna nulla, porttitor ac imperdiet eu, mattis vel lacus.
